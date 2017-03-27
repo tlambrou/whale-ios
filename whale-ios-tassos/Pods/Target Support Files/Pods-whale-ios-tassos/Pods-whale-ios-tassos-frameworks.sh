320b15c75bc92121476f8d37989e2bcd84637ef1
tlambrou <lambrou@gmail.com>
2017-03-27 01:32:53 -0700

    Fixed alamofire network call for login

--- whale-ios-tassos/Pods/Target Support Files/Pods-whale-ios-tassos/Pods-whale-ios-tassos-frameworks.sh
+++ whale-ios-tassos/Pods/Target Support Files/Pods-whale-ios-tassos/Pods-whale-ios-tassos-frameworks.sh
@@ -91,12 +91,14 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Gloss/Gloss.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/KeychainAccess/KeychainAccess.framework"
   install_framework "$BUILT_PRODUCTS_DIR/KeychainSwift/KeychainSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Gloss/Gloss.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/KeychainAccess/KeychainAccess.framework"
   install_framework "$BUILT_PRODUCTS_DIR/KeychainSwift/KeychainSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
 fi
--- whale-ios-tassos/whale-ios-tassos/LoginViewController.swift
+++ whale-ios-tassos/whale-ios-tassos/LoginViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 import Security
 import KeychainSwift
 import Alamofire
+import KeychainAccess
 
 class LoginViewController: ViewController {
   
@@ -27,32 +28,28 @@ class LoginViewController: ViewController {
     let password = passwordTextField.text
     
     
+    let parameters: Parameters? = ["email": email! as String, "password": password! as String]
+    //let headers: HTTPHeaders? = {"Authorization"}
     
-    let parameters: Parameters = ["email": email, "password": password]
+    let url = "https://whale2-elixir.herokuapp.com/api/v1/sessions"
     
-    Alamofire.request("https://whale2-elixir.herokuapp.com/api/v1/sessions", method: .post, parameters: parameters) { response in
+    Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseJSON { response in
       switch response.result {
       case .success(let data):
         print("Successful login network call")
+        self.credentialLabel.textColor = UIColor.green
+        self.credentialLabel.text = "The Credentials Were Correct"
+        print("\n\ncredentials were correct\n\n")
+        self.emailTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
       case .failure:
         print("Network call for login failure")
+        self.credentialLabel.textColor = UIColor.red
+        self.credentialLabel.text = "The Credentials Were Incorrect"
+        print("\n\ncredentials were incorrect\n\n")
+        self.emailTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
       }
-    }
-    
-    if emailTextField.text == email && passwordTextField.text == password {
-      
-      credentialLabel.textColor = UIColor.green
-      credentialLabel.text = "The Credentials Were Correct"
-      print("\n\ncredentials were correct\n\n")
-      emailTextField.resignFirstResponder()
-      passwordTextField.resignFirstResponder()
-    
-    } else {
-      credentialLabel.textColor = UIColor.red
-      credentialLabel.text = "The Credentials Were Incorrect"
-      print("\n\ncredentials were incorrect\n\n")
-      emailTextField.resignFirstResponder()
-      passwordTextField.resignFirstResponder()
     }
     
   }
